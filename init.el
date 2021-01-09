8b1f63187a637cfaafd484360294491ea950ae29
Tim Woods <tim@thewoods.info>
2021-01-09 15:55:42 -0500

    Ensure packages are retrieved on initial setup

--- init.el
+++ init.el
@@ -22,6 +22,7 @@
 
 ;; Set up use package
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
 (setq use-package-verbose t)
 (setq use-package-always-ensure t)
